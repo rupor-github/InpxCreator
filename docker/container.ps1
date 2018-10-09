5e4ac43eeff7df5d53fd1ceacd8b796ecf2646b8
rupor <rupor0mail@gmail.com>
2018-10-09 08:15:48 -0400

    Cleanup

--- docker/container.ps1
+++
@@ -1,6 +0,0 @@
-docker build --squash -t inpxc:latest . -f .\Dockerfile.build | Tee-Object -FilePath "..\..\all.log"
-
-# docker save -o ..\..\inpxc.tar inpxc
-# docker load -i ..\..\inpxc.tar
-
-# docker run --name inpxcreator -v E:/projects/books/InpxCreator:/root/result -it inpxc:latest
