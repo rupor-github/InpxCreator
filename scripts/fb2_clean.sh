f1c679763a5e337fdef9c72dabdbafc7dbf64b44
rupor-github <rupor0mail@gmail.com>
2023-05-28 20:18:28 -0400

    Adding cleanup script

---
+++ scripts/fb2_clean.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Synology task scheduler has a problem running scripts under non-root user
+
+if [ "$2" != "" ]; then
+	user_dir=`eval echo "~$2"`
+	if [ "${user_dir}" != "" ]; then
+    	cd ${user_dir}
+	fi
+fi
+
+# -----------------------------------------------------------------------------
+# Following variables could be changed
+# -----------------------------------------------------------------------------
+
+name="flibusta"
+
+# -----------------------------------------------------------------------------
+# Main body
+# -----------------------------------------------------------------------------
+
+cdate="$(date +%Y%m%d_%H%M%S)"
+mydir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+adir="$1/${name}"
+glog="${mydir}/${name}_clean_${cdate}.log"
+
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3 RETURN
+exec 1>${glog} 2>&1
+
+${mydir}/libclean \
+	--verbose \
+	--destination "${adir}"
+
+res=$?
+if (( ${res} == 1 )); then
+	echo "LIBCLEAN error!"
+	exit 1
+fi
